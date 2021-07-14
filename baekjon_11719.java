59d240a7fdb5ebe33756e77343971dc2e907a4a0
gkdud583 <qqaa5533@naver.com>
2021-07-14 09:50:53 +0900

    baekjon: 11719
    백준 11719: 구현

---
+++ baekjon_11719.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class baekjon_11719{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            System.out.println(sc.nextLine());
+        }
+    }
+}
