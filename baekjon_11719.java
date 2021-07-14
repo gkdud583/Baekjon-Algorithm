f385ddcae6438948b0457db7808f6d6f3a61df6e
gkdud583 <60775067+gkdud583@users.noreply.github.com>
2021-07-14 09:47:36 +0900

    Delete baekjon_11719.java

--- baekjon_11719.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class baekjon_11719{
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()){
-            System.out.println(sc.nextLine());
-        }
-    }
-}
